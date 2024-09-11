05c3d003928eac67d05832361d59c164aa5a464b
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-11 22:13:38 +0200

    GlobalStyle added to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './core/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/App";
+import { GlobalStyle } from "./core/GlobalStyle";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
