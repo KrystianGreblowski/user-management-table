d9ade07fdf96928cbebb81d564c2716a535d1003
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-14 15:19:04 +0200

    media queries added

--- src/features/UsersDataTable/styled.ts
+++ src/features/UsersDataTable/styled.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const TableContainer = styled.table`
   margin: 64px auto;
   padding: 0 8px;
+
+  @media (max-width: 767px) {
+    margin: 32px auto;
+  }
 `;
 
 export const TableHead = styled.thead``;
@@ -17,11 +21,22 @@ export const TableHeader = styled.th`
   padding: 12px;
   box-shadow: 1px 1px 1px #3a3a3a4d;
   border-radius: 3px;
+
+  @media (max-width: 767px) {
+    font-size: 15px;
+    padding: 8px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 13px;
+    padding: 4px;
+  }
 `;
 
 export const TableBody = styled.tbody``;
 
 export const TableData = styled.td`
+  max-width: 300px;
   background-color: white;
   color: black;
   padding: 12px;
@@ -30,4 +45,14 @@ export const TableData = styled.td`
   font-weight: 400;
   box-shadow: 1px 1px 1px #3a3a3a4d;
   border-radius: 3px;
+
+  @media (max-width: 767px) {
+    font-size: 14px;
+    padding: 8px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 10px;
+    padding: 4px;
+  }
 `;
