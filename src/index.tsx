f361aa94f01467c9299a9d01a04a2ec9e54cfec5
Krystian Gręblowski <krystian.greblowski@gmail.com>
2024-09-14 18:06:25 +0200

    Provider added to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./core/App";
 import { GlobalStyle } from "./core/GlobalStyle";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
